cee3980a3373ad2041db1057c16db4c17341b9fc
Matija <matija.karaklic25@gmail.com>
2021-09-26 14:10:16 +0200

    Setup

--- src/me/arteon/lavarun/Events.java
+++ src/me/arteon/lavarun/Events.java
@@ -1,11 +1,16 @@
 package me.arteon.lavarun;
 
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Events implements Listener {
 
     public Events(){
 
+    }
+
+    public void onJoin(PlayerJoinEvent event){
+        event.getPlayer().sendMessage("BABABA");
     }
 
 }
