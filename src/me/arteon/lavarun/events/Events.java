111e9069b65da8e6830a161c0c6a55af763af25f
Matija <matija.karaklic25@gmail.com>
2021-09-27 13:26:47 +0200

    New commands.

--- src/me/arteon/lavarun/events/Events.java
+++ src/me/arteon/lavarun/events/Events.java
@@ -5,6 +5,7 @@ import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class Events implements Listener {
 
